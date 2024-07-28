f0ee02cad492fadd4d251c5921ddebfc9b6b02a3
Santiago Buitrago <jsbuitragorojas@gmail.com>
2024-07-28 13:05:49 -0500

    dbConnect modified

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env.local'
+  );
 }
 
 let cached = global.mongoose;
@@ -18,13 +20,17 @@ async function dbConnect() {
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }).then((mongoose) => {
+    const opts = {
+      bufferCommands: false,
+      // useNewUrlParser: true, // Eliminado porque es obsoleto
+      // useUnifiedTopology: true, // También puedes eliminar esto si no es necesario
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
       return mongoose;
     });
   }
+
   cached.conn = await cached.promise;
   return cached.conn;
 }

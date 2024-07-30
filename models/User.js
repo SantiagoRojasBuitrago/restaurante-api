4cadb52af1a6b993e23f1ac0a6cdd7735292872c
NEGANTIVO <jdmorenov22@gmail.com>
2024-07-29 21:25:15 -0500

    Roles added

--- models/User.js
+++ models/User.js
@@ -15,6 +15,10 @@ const UserSchema = new mongoose.Schema({
     required: true,
     minlength: 6,
   },
+  roles: {
+    type: [String],  // Array de strings para los roles
+    default: ['user'],  // Rol por defecto
+  },
 });
 
 export default mongoose.models.User || mongoose.model('User', UserSchema);
